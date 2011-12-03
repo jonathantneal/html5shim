8b7fb920402b2b4d8cded03f37274b2b3e70aea9
jonathantneal <jonathantneal@hotmail.com>
2011-12-02 17:12:25 -0800

    HTML5 Shim v3
    Major changes:
    - There are now two versions of the HTML5 shim, one which includes the print protector and one which does not.
    - Feature detection for unknown elements.
    - The document methods createElement and createDocumentFragment are now shimmed which, in most cases, allow innerHTML to work correctly.
    - Base styles are included for HTML5 elements. They use minimal selector weight to avoid conflict with your existing stylesheets.
    - Additional elements can be added to the shim in the event that you wish you future-proof your script or add your own fictional elements.
    
    Major changes specific to print:
    - Shimming the CSS no longer increases the selector weight of HTML5 elements (as it did previously).
    - Frames are automatically shimmed during print if they are accessible by the current domain.

---
+++ html5shim-printless.js
@@ -0,0 +1,63 @@
+/*! HTML5 Shim v3.0 | @jon_neal @afarkas @rem | MIT/GPL2 Licensed */
+(function (win, doc) {
+	// feature detection: whether the browser supports unknown elements
+	var supportsUnknownElements = (function (a) { a.innerHTML = '<x-element></x-element>'; return a.childNodes.length === 1; })(doc.createElement('a'));
+
+	// feature detection: whether the browser supports default html5 styles
+	var supportsHtml5Styles = (function (nav, docEl, compStyle) { docEl.appendChild(nav); return (compStyle = (compStyle ? compStyle(nav) : nav.currentStyle).display) && docEl.removeChild(nav) && compStyle === 'block'; })(doc.createElement('nav'), doc.documentElement, win.getComputedStyle);
+
+	// html5 global so that more elements can be shimmed and also so that existing shimming can be detected on iframes
+	// more elements can be added and shimmed with the following code: html5.elements.push('element-name'); shimDocument(document);
+	win.html5 = {
+		// a list of html5 elements
+		elements: 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' '),
+
+		// the shim function
+		shimDocument: function (scopeDocument) {
+			scopeDocument = scopeDocument || doc;
+
+			// test if the document has already been shimmed
+			if (scopeDocument.documentShimmed) return; scopeDocument.documentShimmed = true;
+
+			// set local variables
+			var
+			documentCreateElement = scopeDocument.createElement,
+			documentCreateDocumentFragment = scopeDocument.createDocumentFragment,
+			documentHead = scopeDocument.getElementsByTagName('head')[0],
+			documentCreateElementReplaceFunction = function (m) { documentCreateElement(m); };
+
+			// shim for unknown elements
+			if (!supportsUnknownElements) {
+				// shim the document
+				win.html5.elements.join(' ').replace(/\w+/g, documentCreateElementReplaceFunction);
+
+				// shim document create element function
+				scopeDocument.createElement = function (nodeName) {
+					var element = documentCreateElement(nodeName);
+					if (!/^(input|script)$/i.test(nodeName)) {
+						win.html5.shimDocument(element.document);
+					}
+					return element;
+				};
+
+				// shim document create element function
+				scopeDocument.createDocumentFragment = function () {
+					var documentFragment = documentCreateDocumentFragment();
+					win.html5.shimDocument(documentFragment);
+					return documentFragment;
+				};
+			}
+
+			// shim for default html5 styles
+			if (!supportsHtml5Styles && documentHead) documentHead.insertBefore(documentCreateElement('style'), documentHead.firstChild).styleSheet.cssText = [
+				'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}', // Corrects block display not defined in IE6/7/8/9
+				'canvas,video{display:inline-block;*display:inline;*zoom:1}', // Corrects inline-block display not defined in IE6/7/8/9
+				'[hidden]{display:none}', // Corrects styling for 'hidden' attribute not present in IE7/8/9
+				'mark{background:#FF0;color:#000}' // Addresses styling not present in IE6/7/8/9
+			].join('');
+		}
+	};
+
+	// shim the document
+	win.html5.shimDocument(doc);
+})(this, document);
--- html5shim.js
+++ html5shim.js
@@ -1,6 +1,68 @@
-// Dual licensed under the MIT or GPL Version 2 licenses
-// HTML5 Shim v3.0 @jon_neal @afarkas @rem
+/*! HTML5 Shim v3 | @jon_neal @afarkas @rem | MIT/GPL2 Licensed */
 (function (win, doc) {
+	// feature detection: whether the browser supports unknown elements
+	var supportsUnknownElements = (function (a) { a.innerHTML = '<x-element></x-element>'; return a.childNodes.length === 1; })(doc.createElement('a'));
+
+	// feature detection: whether the browser supports default html5 styles
+	var supportsHtml5Styles = (function (nav, docEl, compStyle) { docEl.appendChild(nav); return (compStyle = (compStyle ? compStyle(nav) : nav.currentStyle).display) && docEl.removeChild(nav) && compStyle === 'block'; })(doc.createElement('nav'), doc.documentElement, win.getComputedStyle);
+
+	// html5 global so that more elements can be shimmed and also so that existing shimming can be detected on iframes
+	// more elements can be added and shimmed with the following code: html5.elements.push('element-name'); shimDocument(document);
+	win.html5 = {
+		// a list of html5 elements
+		elements: 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' '),
+
+		// the shim function
+		shimDocument: function (scopeDocument) {
+			scopeDocument = scopeDocument || doc;
+
+			// test if the document has already been shimmed
+			if (scopeDocument.documentShimmed) return; scopeDocument.documentShimmed = true;
+
+			// set local variables
+			var
+			documentCreateElement = scopeDocument.createElement,
+			documentCreateDocumentFragment = scopeDocument.createDocumentFragment,
+			documentHead = scopeDocument.getElementsByTagName('head')[0],
+			documentCreateElementReplaceFunction = function (m) { documentCreateElement(m); };
+
+			// shim for unknown elements
+			if (!supportsUnknownElements) {
+				// shim the document
+				win.html5.elements.join(' ').replace(/\w+/g, documentCreateElementReplaceFunction);
+
+				// shim document create element function
+				scopeDocument.createElement = function (nodeName) {
+					var element = documentCreateElement(nodeName);
+					if (!/^(input|script)$/i.test(nodeName)) {
+						win.html5.shimDocument(element.document);
+					}
+					return element;
+				};
+
+				// shim document create element function
+				scopeDocument.createDocumentFragment = function () {
+					var documentFragment = documentCreateDocumentFragment();
+					win.html5.shimDocument(documentFragment);
+					return documentFragment;
+				};
+			}
+
+			// shim for default html5 styles
+			if (!supportsHtml5Styles && documentHead) documentHead.insertBefore(documentCreateElement('style'), documentHead.firstChild).styleSheet.cssText = [
+				'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}', // Corrects block display not defined in IE6/7/8/9
+				'canvas,video{display:inline-block;*display:inline;*zoom:1}', // Corrects inline-block display not defined in IE6/7/8/9
+				'[hidden]{display:none}', // Corrects styling for 'hidden' attribute not present in IE7/8/9
+				'mark{background:#FF0;color:#000}' // Addresses styling not present in IE6/7/8/9
+			].join('');
+		}
+	};
+
+	// shim the document
+	win.html5.shimDocument(doc);
+
+	// ie print protector
+
 	// replaces an element with a namespace-shimmed clone (eg. header element becomes shim:header element)
 	function namespaceShimElement(element) {
 		var elementClone = element.document.createElement('shim:' + element.nodeName);
@@ -27,7 +89,8 @@
 		i = -1,
 		cssTextArr = [],
 		styleSheetListLength = styleSheetList.length,
-		styleSheet;
+		styleSheet,
+		styleSheetMediaType;
 
 		// loop through style sheets
 		while (++i < styleSheetListLength) {
@@ -63,165 +126,107 @@
 		while (++i < cssTextSplitLength) {
 			cssTextSplit[i] = cssTextSplit[i].split('}');
 			cssTextSplit[i][cssTextSplit[i].length - 1] = cssTextSplit[i][cssTextSplit[i].length - 1].replace(elementsRegExp, '$1shim\\:$2$3');
-			cssTextSplit[i] = cssTextSplit[i].join('}')
+			cssTextSplit[i] = cssTextSplit[i].join('}');
 		}
 
 		// return shimmed css text
 		return cssTextSplit.join('{');
 	}
 
-	// returns whether the browser supports fictional elements
-	var supportsXElement = (function (a) { a.innerHTML = '<x-element></x-element>'; return a.childNodes.length === 1; })(doc.createElement('a'));
+	// the print shim function
+	function printShimDocument (scopeDocument) {
+		// test for scenarios where shimming is unnecessary or unavailable
+		if (win.html5.supportsXElement || !scopeDocument.namespaces) return;
 
-	// introduces html5 global so that nested iframes can make use of it
-	win.html5 = {
-		// a list of html5 elements
-		elements: 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' '),
+		// add the shim namespace
+		if (!scopeDocument.namespaces.shim) scopeDocument.namespaces.add('shim');
 
-		// the shim function
-		// more elements can be added and shimmed with the following code: html5.elements.push('element-name'); shimDocument(document);
-		shimDocument: function (scopeDocument) {
-			scopeDocument = scopeDocument || doc;
+		// set local variables
+		var
+		i = -1,
+		elementsRegExp = new RegExp('^(' + win.html5.elements.join('|') + ')$', 'i'),
+		nodeList = scopeDocument.getElementsByTagName('*'),
+		nodeListLength = nodeList.length,
+		element,
+		shimmedCSS = shimCssText(getStyleSheetListCssText(scopeDocument.styleSheets));
 
-			// test for scenarios where shimming is unnecessary
-			if (supportsXElement || !scopeDocument || scopeDocument.documentShimmed) return; scopeDocument.documentShimmed = true;
+		// loop through document elements
+		while (++i < nodeListLength) {
+			// get element
+			element = nodeList[i];
 
-			// set local variables
-			var
-			documentCreateElement = scopeDocument.createElement,
-			documentCreateDocumentFragment = scopeDocument.createDocumentFragment,
-			documentHead = scopeDocument.getElementsByTagName('head')[0],
-			documentCreateElementReplaceFunction = function (m) { documentCreateElement(m); };
-
-			// shim the document
-			win.html5.elements.join(' ').replace(/\w+/g, documentCreateElementReplaceFunction);
-
-			// shim document create element function
-			scopeDocument.createElement = function (nodeName) {
-				var element = documentCreateElement(nodeName);
-				if (!/^(input|script)$/i.test(nodeName)) {
-					win.html5.shimDocument(element.document);
-				}
-				return element;
-			};
-
-			// shim document create element function
-			scopeDocument.createDocumentFragment = function () {
-				var documentFragment = documentCreateDocumentFragment();
-				win.html5.shimDocument(documentFragment);
-				return documentFragment;
-			};
-
-			// shim styles
-			if (documentHead) documentHead.insertBefore(documentCreateElement('style'), documentHead.firstChild).styleSheet.cssText = [
-				'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}', // Corrects block display not defined in IE6/7/8/9
-				'canvas,video{display:inline-block;*display:inline;*zoom:1}', // Corrects inline-block display not defined in IE6/7/8/9
-				'[hidden]{display:none}', // Corrects styling for 'hidden' attribute not present in IE7/8/9
-				'abbr[title]{border-bottom:1px dotted}', // Addresses styling not present in IE7/8/9
-				'mark{background:#FF0;color:#000}' // Addresses styling not present in IE6/7/8/9
-			].join('');
-		},
-
-		// the print shim function
-		printShimDocument: function (scopeDocument) {
-			// test for scenarios where shimming is unnecessary or unavailable
-			if (supportsXElement || !scopeDocument.namespaces) return;
-
-			// add the shim namespace
-			if (!scopeDocument.namespaces.shim) scopeDocument.namespaces.add('shim');
-
-			// set local variables
-			var
-			i = -1,
-			elementsRegExp = new RegExp('^(' + win.html5.elements.join('|') + ')$', 'i'),
-			nodeList = scopeDocument.getElementsByTagName('*'),
-			nodeListLength = nodeList.length,
-			element,
-			shimmedCSS = shimCssText(getStyleSheetListCssText(scopeDocument.styleSheets));
-
-			// loop through document elements
-			while (++i < nodeListLength) {
-				// get element
-				element = nodeList[i];
-
-				// clone matching elements as shim namespaced
-				if (elementsRegExp.test(element.nodeName)) {
-					namespaceShimElement(element);
-				}
+			// clone matching elements as shim namespaced
+			if (elementsRegExp.test(element.nodeName)) {
+				namespaceShimElement(element);
 			}
-
-			// set new shimmed css text
-			scopeDocument.appendChild(win.html5._shimmedStyles = scopeDocument.createElement('style')).styleSheet.cssText = shimmedCSS;
-		},
-
-		// the print unshim function
-		unPrintShimDocument: function (scopeDocument) {
-			// test for scenarios where shimming is unnecessary
-			if (supportsXElement || !scopeDocument.namespaces) return;
-
-			// set local variables
-			var
-			i = -1,
-			nodeList = scopeDocument.getElementsByTagName('*'),
-			nodeListLength = nodeList.length,
-			element;
-
-			// loop through document elements
-			while (++i < nodeListLength) {
-				// get element
-				element = nodeList[i];
-
-				// restore original elements
-				if (element.originalElement) {
-					unNamespaceShimElement(element);
-				}
-			}
-
-			// cut new shimmed css text
-			if (win.html5._shimmedStyles) win.html5._shimmedStyles.parentNode.removeChild(win.html5._shimmedStyles);
 		}
-	};
 
-	// shim the document
-	win.html5.shimDocument(doc);
+		// set new shimmed css text
+		scopeDocument.appendChild(win.html5._shimmedStyles = scopeDocument.createElement('style')).styleSheet.cssText = shimmedCSS;
+	}
+
+	// the print unshim function
+	function unPrintShimDocument (scopeDocument) {
+		// test for scenarios where shimming is unnecessary
+		if (win.html5.supportsXElement || !scopeDocument.namespaces) return;
+
+		// set local variables
+		var
+		i = -1,
+		nodeList = scopeDocument.getElementsByTagName('*'),
+		nodeListLength = nodeList.length,
+		element;
+
+		// loop through document elements
+		while (++i < nodeListLength) {
+			// get element
+			element = nodeList[i];
+
+			// restore original elements
+			if (element.originalElement) {
+				unNamespaceShimElement(element);
+			}
+		}
+
+		// cut new shimmed css text
+		if (win.html5._shimmedStyles) win.html5._shimmedStyles.parentNode.removeChild(win.html5._shimmedStyles);
+	}
 
 	// set up print shimming the document
-	if (!supportsXElement) {
+	if (!supportsUnknownElements) {
 		win.onbeforeprint = function () {
 			// set local variables
 			var
 			i = -1,
-			frame,
-			err;
+			frame;
 
 			// printshim any shimable frame documents
 			while ((frame = win.frames[++i])) {
 				try {
-					if (!frame.html5) win.html5.printShimDocument(frame.document);
+					if (!frame.html5) printShimDocument(frame.document);
 				}
 				catch (err) {}
 			}
 
 			// printshim the current document
-			win.html5.printShimDocument(doc);
+			printShimDocument(doc);
 		};
 		win.onafterprint = function () {
 			// set local variables
 			var
 			i = -1,
-			frame,
-			err;
+			frame;
 
 			// unprintshim any shimable frame documents
 			while ((frame = win.frames[++i])) {
 				try {
-					if (!frame.html5) win.html5.unPrintShimDocument(frame.document);
+					if (!frame.html5) unPrintShimDocument(frame.document);
 				}
 				catch (err) {}
 			}
 
 			// unprintshim the current document
-			win.html5.unPrintShimDocument(doc);
-		}
+			unPrintShimDocument(doc);
+		};
 	}
 })(this, document);
